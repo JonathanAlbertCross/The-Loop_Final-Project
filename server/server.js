507d3662cad67dea904fd035f0c3080d02cfa76c
Jonathan Cross <118997615+JonathanAlbertCross@users.noreply.github.com>
2023-11-02 11:51:04 +0000

    server setup

--- server/server.js
+++ server/server.js
@@ -10,4 +10,15 @@ const axios = require("axios");
 
 app.get("/", (_, response) => response.json("Root route for translateApp."));
 
+app.get("/translate", async (request, response) => {
+  const { word, from, to } = request.query;
+  const API = `https://api.mymemory.translated.net/get?q=${word}&langpair=${from}|${to}`;
+  const res = await axios.get(API);
+  const wrangledData = {
+    translation: res.data.responseData.translatedText,
+    match: res.data.responseData.match,
+  };
+  response.json(wrangledData);
+});
+
 app.listen(PORT, () => console.log(`App is running PORT ${PORT}`));
