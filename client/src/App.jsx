b8a137a84ac9c714940606a97d5fe4bdac98fe4a
Vrooty <victoriadohertybone@live.co.uk>
2023-11-14 09:51:44 +0000

    message

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,47 +3,35 @@ import { useState } from "react";
 import axios from "axios";
 
 function App() {
-  const [from, setForm] = useState("en");
-  const [to, setTo] = useState("fr");
+  
+}
 
-  const [word, setWord] = useState("");
-  const [translation, setTranslation] = useState({});
-
-  async function handleTranslate(event) {
-    event.preventDefault();
-    const API = `http://localhost:8080/translate?word=${word}&from=${from}&to=${to}`;
-    const res = await axios.get(API);
-    setTranslation(res.data);
-  }
 
   return (
-    <>
-      <form onSubmit={handleTranslate}>
-        <div className="containerFrom">
-          <select onChange={(event) => setFrom(event.target.value)}>
-            <option value="en">English</option>
-            <option value="fr">Français</option>
-            <option value="es">Espagnol</option>
-          </select>
-          <input
-            placeholder="Translate"
-            onChange={(event) => setWord(event.target.value)}
+    <BrowserRouter>
+      <div className="container">
+        <Header />
+        <div id="login-text">
+          <LoginButton />
+        </div>
+     
+        <Routes>
+          <Route
+            path="/"
+            element={<Home books={books} setBooks={setBooks} deleteBook={deleteBook} />}
           />
-        </div>
-      </form>
-      <form>
-        <div className="containerTo">
-          <select onChange={(event) => setTo(event.target.value)}>
-            <option value="fr">Français</option>
-            <option value="es">Espagnol</option>
-            <option value="en">English</option>
-          </select>
-          <div className="output">{translation.translation}</div>
-        </div>
-        <button>Submit</button>
-      </form>
-    </>
+          <Route path="/about" element={<About />} />
+          <Route path="/catalog" element={<Catalog />} />
+          <Route path="/book/:id" element={<Book />} />
+        </Routes>
+      </div>
+      {/* FOOTER */}
+      <Footer />
+
+    </BrowserRouter>
   );
 }
+
+
 
 export default App;
