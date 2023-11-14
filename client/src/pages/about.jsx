f0606f499d3a6b840cc3e1e6fb75de2270a4358c
Cailum Leyshon <cailumleyshon@gmail.com>
2023-11-14 10:42:40 +0000

    frontend styling (cailum -forgot to do in branch)

--- client/src/pages/about.jsx
+++ client/src/pages/about.jsx
@@ -1,7 +1,7 @@
 export default function About() {
   return (
     <div>
-      <h1>Hello</h1>
+      <h2>Hello</h2>
     </div>
   );
 }
--- client/src/pages/events.jsx
+++ client/src/pages/events.jsx
@@ -1,7 +1,7 @@
 export default function Events() {
   return (
     <div>
-      <h1>Events</h1>
+      <h2>Events</h2>
     </div>
   );
 }
