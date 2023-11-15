2b1066e7a4875b455d805639a1a4cedf4a8acb28
Jonathan Cross <118997615+JonathanAlbertCross@users.noreply.github.com>
2023-11-15 19:25:45 +0000

    grid css

--- client/src/pages/eventsPage.jsx
+++ client/src/pages/eventsPage.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import EventList from "../components/eventList";
 import Form from "../components/Form";
-import "./eventsPage.css";
 
 export default function EventsPage() {
   const [events, setEvents] = useState([]);
