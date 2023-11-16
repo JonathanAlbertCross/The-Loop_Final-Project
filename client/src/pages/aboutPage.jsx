b45e28b21124e0e27bdaf1dc3b455b999cce89f4
Vrooty <victoriadohertybone@live.co.uk>
2023-11-16 13:30:07 +0000

    message

--- client/src/pages/aboutPage.jsx
+++ client/src/pages/aboutPage.jsx
@@ -4,9 +4,9 @@ export default function About() {
 
   return (
   
-      <div className="About-Us">
-        <h2 className="About-People">About The People Who Made This Page</h2>
-        
+     <div className="About-Us">
+       <h2 className="About-People">About The People Who Made This Page</h2>
+        <div className="theTeam">
         <div className="Vicky">
           <img
             src="https://i.ibb.co/Tc9X8kP/20191104-154249.jpg"
@@ -22,7 +22,7 @@ export default function About() {
           </p>
           <p>
             <strong>Why This Project Is Important To Me:</strong>
-             Vienna by Ultravox
+             <p>Vienna by Ultravox</p>
           </p>
         </div>
 
@@ -33,15 +33,18 @@ export default function About() {
           />
           <h3>Jonathan</h3>
           <p>
-            <strong>Location:</strong>  Cambridge
+            <strong>Location:</strong> Cambridge
           </p>
+         
           <p>
             <strong>Hobbies:</strong>  Open water swimming, opera.
           </p>
+          
           <p>
             <strong>Why This Project Is Important To Me:</strong>
-             Having a website for the LGBT community is important to me because it provides a space for connection, support, and empowerment, offering valuable resources, representation, and a sense of belonging in a safe and inclusive environment.
+             <p>Having a website for the LGBT community is important to me because it provides a space for connection, support, and empowerment, offering valuable resources, representation, and a sense of belonging in a safe and inclusive environment.</p>
           </p>
+        
         </div>
 
         <div className="Cailum">
@@ -59,6 +62,8 @@ export default function About() {
             <strong>Why This Project is Important To Me:</strong>
             As a member of the LGBTQ+ community, it is important to have sites like these encouraging and supporting underrepresented members of the tech industry and create spaces and opportunities so that it is the norm for the near future. Having events such as these and seeing people from marginalised communities fostering innovation is inspirational for those new to come in the world of tech.
           </p>
+
+         </div>
         </div>
 
 
